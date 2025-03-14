c14e193bfefea199f089aa120f5c307817adf649
akaseanlo <loeddr@gmail.com>
2025-03-14 20:23:05 +0800

    nothing

--- include/Mario.hpp
+++ include/Mario.hpp
@@ -46,8 +46,8 @@ private:
     float delta_time = 1.0f;
     float run_velocity = 5.0f;
     float jump_velocity = 12.7f;
-    float velocityY = 0.4f;      // 角色在 Y 軸的速度
-    float GRAVITY = -9.8f;  // 重力加速度
+    float velocityY = 0.0f;      // 角色在 Y 軸的速度
+    float GRAVITY = -12.8f;  // 重力加速度
     float JUMP_STRENGTH = 150.0f;  // 跳躍的初速度
     float MAX_JUMP_VELOCITY = 60.0f; // 最高跳躍值
     float additional_jump_forc0 = 0.1f; // 玩家按下按鍵時間，跳躍提升值
